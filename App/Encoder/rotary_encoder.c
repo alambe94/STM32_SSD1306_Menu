b0782cd5ad06d7e89e4c190cedf3cc6f26fa0f14
unknown <you@example.comlambealsaad@gmail.com>
2020-06-04 00:10:33 +0530

    building lcd menu

---
+++ App/Encoder/rotary_encoder.c
@@ -0,0 +1,162 @@
+#include "rotary_encoder.h"
+#include "stddef.h"
+
+// based of
+// multi encoder code with speed up values adapted for STM32-arduino by Matthias Diro
+// no caps for encoder Pins needed: gives back wrong acc values
+// using gpio_read_bit instead of digitalRead gives about 4x more speed http://forums.leaflabs.com/topic.php?id=1107
+
+#define MAX_ENCODERS 3
+
+#define ENCODER_SCAN_TICK 1                           // Encoder_Loop() executes every ENCODER_SCAN_TICK
+#define ENCODER_READ_TICK (250 / ENCODER_SCAN_TICK)   // only read after 100ms of inactivity
+#define ENCODER_RESET_TICK (1000 / ENCODER_SCAN_TICK) // reset encoder count after 500ms of inactivity
+
+static Encoder_Struct_t *Encoder_List[MAX_ENCODERS];
+
+static uint8_t Encoder_Count = 0;
+
+extern uint32_t Encoder_Get_Tick();
+
+uint8_t Encoder_Add(Encoder_Struct_t *handle)
+{
+    if (Encoder_Count < MAX_ENCODERS)
+    {
+        if (handle->Encoder_Init)
+        {
+            handle->Encoder_Init();
+        }
+        handle->Encoder_Time_Stamp = 0;
+        handle->Encoder_Count = 0;
+
+        /* initial state of encoder pins */
+        if (handle->Encoder_Read_Pin_A && handle->Encoder_Read_Pin_B)
+        {
+            handle->Encoder_Pin_A_State = handle->Encoder_Read_Pin_A();
+            handle->Encoder_Pin_B_State = handle->Encoder_Read_Pin_B();
+        }
+
+        Encoder_List[Encoder_Count] = handle;
+        Encoder_Count++;
+
+        return (Encoder_Count - 1); //return encoder ID
+    }
+    else
+    {
+        //Error
+        return (255); //return error
+    }
+}
+
+/* call in millis callback or systick callback */
+void Encoder_Loop()
+{
+    static uint32_t Encoder_Scan_Time_Stamp = 0;
+    Encoder_Struct_t *handle = NULL;
+    uint8_t pin_a_new_state = 0;
+    uint8_t pin_b_new_state = 0;
+
+    /* execute this loop on every ENCODER_SCAN_TICK */
+    if (Encoder_Get_Tick() - Encoder_Scan_Time_Stamp > ENCODER_SCAN_TICK)
+    {
+        Encoder_Scan_Time_Stamp = Encoder_Get_Tick();
+
+        for (uint8_t Index = 0; Index < Encoder_Count; Index++)
+        {
+            handle = Encoder_List[Index];
+
+            pin_a_new_state = handle->Encoder_Read_Pin_A();
+            pin_b_new_state = handle->Encoder_Read_Pin_B();
+
+            /*current state != previous*/
+            if (pin_a_new_state != handle->Encoder_Pin_A_State)
+            {
+                handle->Encoder_Pin_A_State = pin_a_new_state;
+
+                if (handle->Encoder_Pin_A_State != handle->Encoder_Pin_B_State)
+                {
+                    if (Encoder_Get_Tick() - handle->Encoder_Time_Stamp > 10)
+                    {
+                        handle->Encoder_Count += 1;
+                    }
+                    else if (Encoder_Get_Tick() - handle->Encoder_Time_Stamp > 5)
+                    {
+                        handle->Encoder_Count += 10;
+                    }
+                    else
+                    {
+                        handle->Encoder_Count += 50;
+                    }
+
+                    handle->Encoder_Time_Stamp = Encoder_Get_Tick();
+                }
+            }
+
+            if (pin_b_new_state != handle->Encoder_Pin_B_State)
+            {
+                handle->Encoder_Pin_B_State = pin_b_new_state;
+
+                if (handle->Encoder_Pin_B_State != handle->Encoder_Pin_A_State)
+                {
+                    if (Encoder_Get_Tick() - handle->Encoder_Time_Stamp > 10)
+                    {
+                        handle->Encoder_Count -= 1;
+                    }
+                    else if (Encoder_Get_Tick() - handle->Encoder_Time_Stamp > 5)
+                    {
+                        handle->Encoder_Count -= 10;
+                    }
+                    else
+                    {
+                        handle->Encoder_Count -= 50;
+                    }
+
+                    handle->Encoder_Time_Stamp = Encoder_Get_Tick();
+                }
+            }
+        }
+    }
+}
+
+int16_t Encoder_Get_Count(Encoder_Struct_t *handle)
+{
+    int16_t count = 0;
+
+    if (handle != NULL)
+    {
+        if (Encoder_Get_Tick() - handle->Encoder_Time_Stamp > 500)
+        {
+            /* reset encoder count after 500ms of inactivity*/
+            handle->Encoder_Count = 0;
+        }
+        else if (Encoder_Get_Tick() - handle->Encoder_Time_Stamp > 100)
+        {
+            /* copy count, only after 100ms of inactivity*/
+            count = handle->Encoder_Count;
+            handle->Encoder_Count = 0;
+        }
+        else
+        {
+            /* do nothing, encoder is still active */
+        }
+    }
+
+    return count;
+}
+
+/* to reset*/
+void Encoder_Reset_Count(Encoder_Struct_t *handle)
+{
+    if (handle != NULL)
+    {
+        handle->Encoder_Count = 0;
+    }
+}
+
+void Encoder_Set_Count(Encoder_Struct_t *handle, int16_t count)
+{
+    if (handle != NULL)
+    {
+        handle->Encoder_Count = count;
+    }
+}
---
+++ App/app_main.c
@@ -0,0 +1,121 @@
+#include "stdio.h"
+#include "main.h"
+#include "usart.h"
+#include "rotary_encoder.h"
+#include "button.h"
+#include "menu_core.h"
+#include "menu_page0.h"
+#include "menu_page1.h"
+#include "ssd1306.h"
+#include "fonts.h"
+
+Encoder_Struct_t Encoder_X;
+Button_Struct_t Button_X;
+
+/* redirect printf to uart */
+int __io_putchar(int ch)
+{
+    huart2.Instance->DR = (ch);
+    while (__HAL_UART_GET_FLAG(&huart2, UART_FLAG_TC) == 0)
+        ;
+    return ch;
+}
+
+/** Encoder Functions Start ***/
+uint32_t Encoder_Get_Tick()
+{
+    return HAL_GetTick();
+}
+
+void Encoder_X_Init()
+{
+    // enable clock, configure as gpio as input
+    // configured in cube @see gpio.c
+}
+
+uint8_t Encoder_X_Read_Pin_A()
+{
+    return HAL_GPIO_ReadPin(ENC_X_A_GPIO_Port, ENC_X_A_Pin);
+}
+
+uint8_t Encoder_X_Read_Pin_B()
+{
+    return HAL_GPIO_ReadPin(ENC_X_B_GPIO_Port, ENC_X_B_Pin);
+}
+/** Encoder Functions End ***/
+
+/** Button Functions  Start ***/
+uint32_t Button_Get_Tick()
+{
+    return HAL_GetTick();
+}
+
+void Button_X_Init()
+{
+    // enable clock, configure as gpio as input
+    // configured in cube @see gpio.c
+}
+
+uint8_t Button_X_Read()
+{
+    return HAL_GPIO_ReadPin(ENC_BT_GPIO_Port, ENC_BT_Pin) ? 0 : 1;
+}
+/** Button Functions End ***/
+
+/** Menu Functions Start ***/
+uint32_t Menu_Get_Tick()
+{
+    return HAL_GetTick();
+}
+
+void Menu_Get_Event(Menu_Event_t *event)
+{
+    event->Encoder_Count = Encoder_Get_Count(&Encoder_X);
+    event->Enter_Button_Clicks = Button_Get_Clicked_Count(&Button_X);
+    event->Back_Button_Clicks = 0;
+    event->Down_Button_Clicks = 0;
+    event->Up_Button_Clicks = 0;
+    event->Right_Button_Clicks = 0;
+    event->Left_Button_Clicks = 0;
+}
+/** Menu Functions End ***/
+
+void Sytick_Callback()
+{
+    Encoder_Loop();
+    Button_Loop();
+}
+
+void App_Main()
+{
+    /* io buffer off*/
+    /* redirect printf to uart */
+    setvbuf(stdout, NULL, _IONBF, 0);
+
+    Encoder_X.Encoder_Init = Encoder_X_Init;
+    Encoder_X.Encoder_Read_Pin_A = Encoder_X_Read_Pin_A;
+    Encoder_X.Encoder_Read_Pin_B = Encoder_X_Read_Pin_B;
+    Encoder_Add(&Encoder_X);
+
+    /* button call is not used, set to NULL */
+    Button_X.Button_Init = Button_X_Init;
+    Button_X.Button_Read = Button_X_Read;
+    Button_X.Callback = NULL;
+    Button_Add(&Button_X);
+
+    ssd1306_Init();
+
+    ssd1306_Fill(Black);
+    ssd1306_SetCursor(0, 0);
+    ssd1306_WriteString("SSD1306 Menu", Font_7x10, White);
+    ssd1306_UpdateScreen();
+
+    Menu_Page0_Init();
+    Menu_Page1_Init();
+    Menu_Change_Page(0, 0); // by default show page0 screen0.
+
+    while (1)
+    {
+        Menu_Loop();
+    }
+}
